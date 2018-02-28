c4f6cd498c7f29c85c966e865b3d2f53a5a6ee62
Kograth <31609373+Kograth@users.noreply.github.com>
2018-02-28 17:51:44 +0300

    Обратная связь.Создание топика под каждый девайс.

--- src/main/java/communication/ResponseToDevices.java
+++ src/main/java/communication/ResponseToDevices.java
@@ -4,13 +4,17 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 
+//Обработка входящих сообщений от приложений (1С например)
 public class ResponseToDevices implements Processor {
+
+
     @Override
     public void process(Exchange exchange) throws Exception {
 
+        String NS = new String(exchange.getIn().getBody(String.class));
         Message out = exchange.getOut();
-        out.setHeader("foo","MyDeviceTest");
-        out.setBody("I am train to work.");
+        out.setHeader("IDDevice","ID1-001-001-001-001");
+        out.setBody("Something haping in here...");
 
     }
 }
--- src/main/java/ru/cse/ExchangeBetweenDevices.java
+++ src/main/java/ru/cse/ExchangeBetweenDevices.java
@@ -1,13 +1,15 @@
 package ru.cse;
 
 import communication.ResponseToDevices;
+import org.apache.camel.Header;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.ValueBuilder;
 
 public class ExchangeBetweenDevices extends RouteBuilder{
     @Override
     public void configure() throws Exception {
 
-////      ДЛЯ ТЕСТИРОВАНИЯ
+////      ДЛЯ ТЕСТИРОВАНИЯ SQL БАЗЫ
 //       from("timer:foo?period=10s")
 //                .setBody(simple("2018-02-01 13:04:00\\t45.2\\t23.2\\t456\\t1\\tgo\\tRussia\\tMotorolaZX8"))
 //                .to("mqtt:cheese?publishTopicName=GeoDataV2&userName=smx&password=smx")
@@ -21,11 +23,12 @@ public class ExchangeBetweenDevices extends RouteBuilder{
                 .to("sql:insert into Geodata values (:#RegData,:#lon,:#lat,CONVERT(BINARY(16),:#UIDTask),:#UIDTaskType,:#StateTask,:#Geography,:#Device2)")
                 .to("log:Delivery to SQL");
 
-        from("timer://foo?fixedRate=true;period=10000")
-                .process(new ResponseToDevices())
-                .to("activemq:topic:VirtualTopic.Orders").to("log:Read msg from 1C");
 
-        from("activemq:Consumer.A.VirtualTopic.Orders").to("activemq:queue:AnswerMe");
+        from("activemq:queue:Devices.MessageFrom1C")
+                .process(new ResponseToDevices())
+                .toD("activemq:topic:${header.IDDevice}").to("log:Read msg from 1C");
+
+
     }
 
 }
