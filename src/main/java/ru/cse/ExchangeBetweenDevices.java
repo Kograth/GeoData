5a90f6bbd2ba4a5912c2bedf80aeed65a95b899c
Kograth <Lancer9>
2018-05-28 15:52:13 +0300

    Синтаксическая ошибка записи в SQL.

--- src/main/java/ru/cse/ExchangeBetweenDevices.java
+++ src/main/java/ru/cse/ExchangeBetweenDevices.java
@@ -19,7 +19,7 @@ public class ExchangeBetweenDevices extends RouteBuilder{
         from("activemq:topic:GeoDataV2").process(new SavingDataToDatabase())
                 .choice()
                 .when(header("No data").isEqualTo("0")).to("log:No data to write DataBase. Lenght -- :#lenghtM").otherwise()
-                .to("sql:insert into dbo.GeoData values (:#RegData,:#lon,:#lat,CONVERT(BINARY(16),:#UIDTask,:#UIDTaskType,:#StateTask,:#Geography,:#Device2)")
+                .to("sql:insert into dbo.GeoData values (:#RegData,:#lon,:#lat,CONVERT(BINARY(16),:#UIDTask),:#UIDTaskType,:#StateTask,:#Geography,:#Device2)")
                 .to("log:Delivery to SQL");
 
 
